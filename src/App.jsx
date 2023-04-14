d138c76a5f2fb85839f9e61c75ac48601f7a9147
Colibryo <limagnin75@gmail.com>
2023-04-14 18:03:37 +0200

    Initialisation du projet form-test avec react; utilisation de font-family Noto sans

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello World</h1>  
+      </header>
+      <main>
+      </main> 
+      <footer></footer>
+    </div>
+  );
+}
+
+export default App;
